a1309f49cd19bf5d47719da4e440b3508fafcd35
Mykola Kushka <Mykola.Kushka@gmail.com>
2021-07-06 23:30:26 +0300

    Select months

--- js/calendar.js
+++ js/calendar.js
@@ -2,7 +2,13 @@ let calendarArea = document.querySelector('#calendar');
 let calendarCode = '';
 let today = new Date();
 let currentMonth = today.getMonth();
+let monthNumber = currentMonth;
 let currentYear = today.getFullYear();
+let shownYear = currentYear;
+let monthsQuantitySelector = document.querySelector(
+  'select#monthsQuantitySelector'
+);
+let monthsQuantity = monthsQuantitySelector.value;
 
 const Months = new Array(
   'January',
@@ -26,6 +32,11 @@ function daysInMonth(iMonth, iYear) {
   return 32 - new Date(iYear, iMonth, 32).getDate();
 }
 
+// Show year
+function showYearTitle() {
+  calendarCode += `<div class="calendar-header"><div class="year-title" colspan="7">${shownYear}</div></div>`;
+}
+
 // Build days in month table
 function showDaysInMonth(month, year) {
   let date = 1;
@@ -39,7 +50,7 @@ function showDaysInMonth(month, year) {
         calendarCode += '<td></td>';
       } else if (date > daysInMonth(month, year)) {
       } else {
-        calendarCode += `<td>${date}</td>`;
+        calendarCode += `<td class="day">${date}</td>`;
         date++;
       }
     }
@@ -49,14 +60,46 @@ function showDaysInMonth(month, year) {
 }
 
 // Build one month
-calendarCode += `<table>
-  <tr><td colspan="7">${currentYear}</td></tr>
-  <tr><td colspan="7">${Months[currentMonth]}</td></tr><tr>`;
+function buildMonths(months) {
+  showYearTitle();
 
-Days.forEach((item) => (calendarCode += `<td>${item}</td>`));
+  calendarCode += '<div class="months-container">';
 
-showDaysInMonth(currentMonth, currentYear);
+  for (let i = 0; i < months; i++) {
+    calendarCode += `<table class="month">
+  <thead>
+  <tr><th class="month-title" colspan="7">${Months[monthNumber]}</th></tr><tr>`;
 
-calendarCode += `</tr></table>`;
+    Days.forEach(
+      (item) => (calendarCode += `<th class="day-header">${item}</th>`)
+    );
 
-calendarArea.innerHTML = calendarCode;
+    calendarCode += `</tr></thead><tbody>`;
+
+    showDaysInMonth(monthNumber, shownYear);
+
+    if (monthNumber < 11) {
+      monthNumber++;
+    } else {
+      monthNumber = 0;
+      shownYear++;
+    }
+
+    calendarCode += `</tbody></tr></table>`;
+  }
+
+  calendarCode += '</div>';
+
+  calendarArea.innerHTML = calendarCode;
+}
+
+buildMonths(monthsQuantity);
+
+$('#monthsQuantitySelector').on('change', (e) => {
+  calendarCode = '';
+  monthNumber = currentMonth;
+  shownYear = currentYear;
+  monthsQuantity = e.target.value;
+
+  buildMonths(monthsQuantity);
+});
