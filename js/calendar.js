821b3c4470f9e6e0848c6a55c8fa5689b281c147
Mykola Kushka <Mykola.Kushka@gmail.com>
2021-07-22 12:30:44 +0300

    JSON replaced

--- js/calendar.js
+++ js/calendar.js
@@ -1,3 +1,250 @@
+// Days off
+// let daysOff = [
+//   {
+//     id: 0,
+//     color: '#f2c94c',
+//     name: 'Cameron Williamson',
+//     periods: [
+//       {
+//         start: '2021-06-28',
+//         end: '2021-07-06',
+//       },
+//       {
+//         start: '2021-08-09',
+//         end: '2021-08-18',
+//       },
+//       {
+//         start: '2021-09-23',
+//         end: '2021-09-29',
+//       },
+//     ],
+//   },
+//   {
+//     id: 1,
+//     color: '#F78F1E',
+//     name: 'Ronald Richards',
+//     periods: [
+//       {
+//         start: '2021-09-27',
+//         end: '2021-10-01',
+//       },
+//     ],
+//   },
+//   {
+//     id: 2,
+//     color: '#56ccf2',
+//     name: 'Jacob Jones',
+//     periods: [
+//       {
+//         start: '2021-07-19',
+//         end: '2021-07-23',
+//       },
+//     ],
+//   },
+//   {
+//     id: 3,
+//     color: '#bb6bd9',
+//     name: 'Bessie Cooper',
+//     periods: [
+//       {
+//         start: '2021-11-14',
+//         end: '2021-11-20',
+//       },
+//     ],
+//   },
+//   {
+//     id: 4,
+//     color: '#6fcf97',
+//     name: 'Devon Lane',
+//     periods: [
+//       {
+//         start: '2021-09-01',
+//         end: '2021-09-13',
+//       },
+//     ],
+//   },
+// ];
+
+let daysOff = {
+  Data: [
+    {
+      EmployeeId: '97005000',
+      EmployeeName: 'Denise Flanders',
+      AbsenceTypeId: null,
+      AbsenceStartDate: '2021-07-15T00:00:00',
+      AbsenceFinishDate: '2021-07-22T00:00:00',
+      AbsenceDescription: 'Annual Leave',
+      AbsenceHalfStartDay: false,
+      AbsenceHalfFinishDay: false,
+      AbsenceRequestedDays: 6.0,
+      AbsenceRequestedHours: 46.8,
+      AbsenceState: 'Submitted',
+      AbsenceLabelColor: '2CA4BF',
+      AbsenceTextColor: 'ffffff',
+    },
+    {
+      EmployeeId: '97005035',
+      EmployeeName: 'Christina Hunter',
+      AbsenceTypeId: null,
+      AbsenceStartDate: '2021-01-05T00:00:00',
+      AbsenceFinishDate: '2021-01-13T00:00:00',
+      AbsenceDescription: 'Annual Leave',
+      AbsenceHalfStartDay: false,
+      AbsenceHalfFinishDay: false,
+      AbsenceRequestedDays: 7.0,
+      AbsenceRequestedHours: 54.6,
+      AbsenceState: 'Approved',
+      AbsenceLabelColor: '8CD19D',
+      AbsenceTextColor: 'ffffff',
+    },
+    {
+      EmployeeId: '97005035',
+      EmployeeName: 'Christina Hunter',
+      AbsenceTypeId: null,
+      AbsenceStartDate: '2021-07-20T00:00:00',
+      AbsenceFinishDate: '2021-07-22T00:00:00',
+      AbsenceDescription: 'Annual Leave',
+      AbsenceHalfStartDay: false,
+      AbsenceHalfFinishDay: false,
+      AbsenceRequestedDays: 3.0,
+      AbsenceRequestedHours: 23.4,
+      AbsenceState: 'Submitted',
+      AbsenceLabelColor: '8CD19D',
+      AbsenceTextColor: 'ffffff',
+    },
+    {
+      EmployeeId: '97005035',
+      EmployeeName: 'Christina Hunter',
+      AbsenceTypeId: null,
+      AbsenceStartDate: '2021-04-14T00:00:00',
+      AbsenceFinishDate: '2021-04-15T00:00:00',
+      AbsenceDescription: 'Annual Leave',
+      AbsenceHalfStartDay: false,
+      AbsenceHalfFinishDay: false,
+      AbsenceRequestedDays: 2.0,
+      AbsenceRequestedHours: 15.6,
+      AbsenceState: 'Approved',
+      AbsenceLabelColor: '8CD19D',
+      AbsenceTextColor: 'ffffff',
+    },
+    {
+      EmployeeId: '97005039',
+      EmployeeName: 'Richard Hynes',
+      AbsenceTypeId: null,
+      AbsenceStartDate: '2021-01-04T00:00:00',
+      AbsenceFinishDate: '2021-01-08T00:00:00',
+      AbsenceDescription: 'Annual Leave',
+      AbsenceHalfStartDay: false,
+      AbsenceHalfFinishDay: false,
+      AbsenceRequestedDays: 5.0,
+      AbsenceRequestedHours: 39.0,
+      AbsenceState: 'Approved',
+      AbsenceLabelColor: '89bdd3',
+      AbsenceTextColor: 'ffffff',
+    },
+    {
+      EmployeeId: '97005069',
+      EmployeeName: 'John Dowling',
+      AbsenceTypeId: null,
+      AbsenceStartDate: '2021-07-19T00:00:00',
+      AbsenceFinishDate: '2021-07-19T00:00:00',
+      AbsenceDescription: 'Absent Paid Leave',
+      AbsenceHalfStartDay: false,
+      AbsenceHalfFinishDay: false,
+      AbsenceRequestedDays: 1.0,
+      AbsenceRequestedHours: 7.8,
+      AbsenceState: 'Approved',
+      AbsenceLabelColor: 'e05038',
+      AbsenceTextColor: 'ffffff',
+    },
+    {
+      EmployeeId: '97005069',
+      EmployeeName: 'John Dowling',
+      AbsenceTypeId: null,
+      AbsenceStartDate: '2021-01-04T00:00:00',
+      AbsenceFinishDate: '2021-01-08T00:00:00',
+      AbsenceDescription: 'Annual Leave',
+      AbsenceHalfStartDay: false,
+      AbsenceHalfFinishDay: false,
+      AbsenceRequestedDays: 5.0,
+      AbsenceRequestedHours: 39.0,
+      AbsenceState: 'Approved',
+      AbsenceLabelColor: 'e05038',
+      AbsenceTextColor: 'ffffff',
+    },
+    {
+      EmployeeId: '97005069',
+      EmployeeName: 'John Dowling',
+      AbsenceTypeId: null,
+      AbsenceStartDate: '2021-01-04T00:00:00',
+      AbsenceFinishDate: '2021-01-04T00:00:00',
+      AbsenceDescription: 'Annual Leave',
+      AbsenceHalfStartDay: false,
+      AbsenceHalfFinishDay: false,
+      AbsenceRequestedDays: 1.0,
+      AbsenceRequestedHours: 7.8,
+      AbsenceState: 'Approved',
+      AbsenceLabelColor: 'e05038',
+      AbsenceTextColor: 'ffffff',
+    },
+  ],
+  AbsenceCalendarShiftType: '4',
+  AbsenceRequestedDaysOrHours: 'Days',
+  AbsenceDataCutOffYear: 2016,
+  AbsenceCalendarFilter: [
+    {
+      Text: 'Denise Flanders',
+      Value: 'EmployeeId_97005000',
+      LabelColor: '2CA4BF',
+      TextColor: 'ffffff',
+      Id: 'AbsenceType_EmployeeId_97005000',
+    },
+    {
+      Text: 'Christina Hunter',
+      Value: 'EmployeeId_97005035',
+      LabelColor: '8CD19D',
+      TextColor: 'ffffff',
+      Id: 'AbsenceType_EmployeeId_97005035',
+    },
+    {
+      Text: 'Richard Hynes',
+      Value: 'EmployeeId_97005039',
+      LabelColor: '89bdd3',
+      TextColor: 'ffffff',
+      Id: 'AbsenceType_EmployeeId_97005039',
+    },
+    {
+      Text: 'John Dowling',
+      Value: 'EmployeeId_97005069',
+      LabelColor: 'e05038',
+      TextColor: 'ffffff',
+      Id: 'AbsenceType_EmployeeId_97005069',
+    },
+  ],
+  CalendarNationalHolidays: [
+    '2021-01-01T00:00:00',
+    '2021-03-17T00:00:00',
+    '2021-04-05T00:00:00',
+    '2021-05-03T00:00:00',
+    '2021-06-07T00:00:00',
+    '2021-08-02T00:00:00',
+    '2021-10-25T00:00:00',
+    '2021-12-27T00:00:00',
+    '2021-12-28T00:00:00',
+    '2022-01-03T00:00:00',
+    '2022-03-17T00:00:00',
+    '2022-04-18T00:00:00',
+    '2022-05-02T00:00:00',
+    '2022-06-06T00:00:00',
+    '2022-08-01T00:00:00',
+    '2022-10-31T00:00:00',
+    '2022-12-26T00:00:00',
+    '2022-12-27T00:00:00',
+  ],
+  IsThereOverlappingAbsence: true,
+  ErrorMessage: null,
+};
+
 let calendarArea = document.querySelector('#calendar');
 let calendarCode = '';
 let monthsQuantitySelector = document.querySelector(
@@ -156,73 +403,6 @@ function addMonthsHandler() {
   });
 }
 
-// Days off
-let daysOff = [
-  {
-    id: 0,
-    color: '#f2c94c',
-    name: 'Cameron Williamson',
-    periods: [
-      {
-        start: '2021-06-28',
-        end: '2021-07-06',
-      },
-      {
-        start: '2021-08-09',
-        end: '2021-08-18',
-      },
-      {
-        start: '2021-09-23',
-        end: '2021-09-29',
-      },
-    ],
-  },
-  {
-    id: 1,
-    color: '#F78F1E',
-    name: 'Ronald Richards',
-    periods: [
-      {
-        start: '2021-09-27',
-        end: '2021-10-01',
-      },
-    ],
-  },
-  {
-    id: 2,
-    color: '#56ccf2',
-    name: 'Jacob Jones',
-    periods: [
-      {
-        start: '2021-07-19',
-        end: '2021-07-23',
-      },
-    ],
-  },
-  {
-    id: 3,
-    color: '#bb6bd9',
-    name: 'Bessie Cooper',
-    periods: [
-      {
-        start: '2021-11-14',
-        end: '2021-11-20',
-      },
-    ],
-  },
-  {
-    id: 4,
-    color: '#6fcf97',
-    name: 'Devon Lane',
-    periods: [
-      {
-        start: '2021-09-01',
-        end: '2021-09-13',
-      },
-    ],
-  },
-];
-
 // Checkbox filters
 let calendarFilter = document.querySelector('#calendarFilter');
 let filtersCode = '';
@@ -238,11 +418,11 @@ function addCheckbox(name, color, id) {
 
   cssCheckboxesColors += `
     .calendars-checkbox .mycheckbox-2.bgcolor-${id} input:checked + span::before{
-      background-color: ${color};
+      background-color: #${color};
     }
 
     .calendars-checkbox .mycheckbox-2.bgcolor-${id} span::before{
-      border-color: ${color};
+      border-color: #${color};
     }`;
 }
 
@@ -293,78 +473,86 @@ function showDaysOff() {
 
   let _startDate = new Date(startDate);
   _startDate.setHours(0, 0, 0, 0);
-  daysOff.forEach((item) => {
+  daysOff.Data.forEach((item) => {
     let checkbox = document.querySelector(
-      `#calendarFilter #employee-${item.id}`
+      `#calendarFilter #employee-${item.EmployeeId}`
     );
     if (!checkbox) {
-      addCheckbox(item.name, item.color, item.id);
+      addCheckbox(item.EmployeeName, item.AbsenceLabelColor, item.EmployeeId);
     }
 
     // Check if employee checkbox is checked
-    let employeeCheckboxId = `#employee-${item.id}`;
+    let employeeCheckboxId = `#employee-${item.EmployeeId}`;
 
     if ($(employeeCheckboxId)[0].checked) {
       isFirstDay = false;
       isLastDay = false;
-      if (employeeId != item.id) {
-        employeeId = item.id;
+      if (employeeId != item.EmployeeId) {
+        employeeId = item.EmployeeId;
       }
-      color = item.color;
-      item.periods.forEach((period) => {
-        isStartInPeriod = false;
-        isEndInPeriod = false;
-        startOffDate = new Date(period.start);
-        startOffDate.setHours(0, 0, 0, 0);
-        endOffDate = new Date(period.end);
-        endOffDate.setHours(23, 59, 59, 999);
+      color = item.AbsenceLabelColor;
+      isStartInPeriod = false;
+      isEndInPeriod = false;
+      startOffDate = new Date(item.AbsenceStartDate.slice(0, 10));
+      startOffDate.setHours(0, 0, 0, 0);
+      endOffDate = new Date(item.AbsenceFinishDate.slice(0, 10));
+      endOffDate.setHours(23, 59, 59, 999);
 
-        if (startOffDate >= _startDate && startOffDate <= endDate) {
-          isStartInPeriod = true;
-          isFirstDay = true;
-        }
+      if (startOffDate >= _startDate && startOffDate <= endDate) {
+        isStartInPeriod = true;
+        isFirstDay = true;
+      }
 
-        if (endOffDate >= _startDate && endOffDate <= endDate) {
-          isEndInPeriod = true;
-          isLastDay = true;
-        }
+      if (endOffDate >= _startDate && endOffDate <= endDate) {
+        isEndInPeriod = true;
+        isLastDay = true;
+      }
 
-        if (isStartInPeriod && !isEndInPeriod) {
-          endOffDate = new Date(endDate);
-          isEndInPeriod = true;
-        }
+      if (isStartInPeriod && !isEndInPeriod) {
+        endOffDate = new Date(endDate);
+        isEndInPeriod = true;
+      }
 
-        if (!isStartInPeriod && isEndInPeriod) {
-          startOffDate = new Date(_startDate);
-          isStartInPeriod = true;
-        }
+      if (!isStartInPeriod && isEndInPeriod) {
+        startOffDate = new Date(_startDate);
+        isStartInPeriod = true;
+      }
 
-        if (isStartInPeriod && isEndInPeriod) {
-          for (
-            let day = new Date(startOffDate);
-            day <= endOffDate;
-            day.setDate(day.getDate() + 1)
+      if (isStartInPeriod && isEndInPeriod) {
+        for (
+          let day = new Date(startOffDate);
+          day <= endOffDate;
+          day.setDate(day.getDate() + 1)
+        ) {
+          _day = day.getDate() < 10 ? '0' + day.getDate() : day.getDate();
+          _month = day.getMonth();
+          _month = _month + 1;
+          _month = _month < 10 ? '0' + _month : _month;
+          id = `d-${day.getFullYear()}-${_month}-${_day}`;
+
+          if (document.getElementById(id).style.backgroundColor) {
+            document.getElementById(id).style = '';
+            document.getElementById(id).classList.add('day-multi');
+          } else
+            document.getElementById(id).style.backgroundColor = `#${color}`;
+
+          // Add styles for first and last day of period
+          if (
+            _day == startOffDate.getDate() &&
+            isFirstDay &&
+            _day == endOffDate.getDate() &&
+            isLastDay
           ) {
-            _day = day.getDate() < 10 ? '0' + day.getDate() : day.getDate();
-            _month = day.getMonth();
-            _month = _month + 1;
-            _month = _month < 10 ? '0' + _month : _month;
-            id = `d-${day.getFullYear()}-${_month}-${_day}`;
-
-            if (document.getElementById(id).style.backgroundColor) {
-              document.getElementById(id).style = '';
-              document.getElementById(id).classList.add('day-multi');
-            } else document.getElementById(id).style.backgroundColor = color;
-
-            // Add styles for first and last day of period
-            if (_day == startOffDate.getDate() && isFirstDay) {
-              document.getElementById(id).classList.add('day-first');
-            } else if (_day == endOffDate.getDate() && isLastDay) {
-              document.getElementById(id).classList.add('day-last');
-            }
+            console.log(id);
+            document.getElementById(id).classList.add('day-first');
+            document.getElementById(id).classList.add('day-last');
+          } else if (_day == startOffDate.getDate() && isFirstDay) {
+            document.getElementById(id).classList.add('day-first');
+          } else if (_day == endOffDate.getDate() && isLastDay) {
+            document.getElementById(id).classList.add('day-last');
           }
         }
-      });
+      }
     }
   });
 
@@ -405,7 +593,7 @@ function hoverHandler() {
 function showTooltip(el) {
   let tooltip = document.createElement('div');
   tooltip.className = 'day-tooltip';
-  tooltip.innerHTML = 'Text';
+  tooltip.innerHTML = el.id;
   el.append(tooltip);
 }
 
